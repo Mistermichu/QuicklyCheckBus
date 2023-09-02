e3ab7abdc262640e742bba3adf9abcf60be2f6dc
MichałM <michalhm@gmail.com>
2023-09-02 22:31:24 +0200

    New class for Routes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from StopSelecter import StopSelecter
 
 URL_STOPS = "http://api.zdiz.gdynia.pl/pt/stops"
 URL_DELAYS = "http://api.zdiz.gdynia.pl/pt/delays?stopId={stop_id}"
+URL_ROUTES = "http://api.zdiz.gdynia.pl/pt/routes"
 
 stop = StopSelecter(URL_STOPS)
-
+stop.get_stop_data()
+print(stop.stop_data["stopId"])
