0f0de88f1a23e113192f76942135890f6d59528e
MichałM <michalhm@gmail.com>
2023-09-02 20:13:53 +0200

    StopSelecter, select stop

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import requests
-import json
-import sys
 from StopSelecter import StopSelecter
 
 
 URL_STOPS = "http://api.zdiz.gdynia.pl/pt/stops"
 URL_DELAYS = "http://api.zdiz.gdynia.pl/pt/delays?stopId={stop_id}"
+
+stop = StopSelecter(URL_STOPS)
+stop.stop_name
 
